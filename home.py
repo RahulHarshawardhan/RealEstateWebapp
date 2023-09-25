429c491a5df8db7c6ec18f58b918cd4c2448556a
Rahul Harshawardhan <rahulharshawardhan@yahoo.co.in>
2023-09-25 18:40:52 +0530

    Recommendation system done

--- home.py
+++ home.py
@@ -1,11 +1,24 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="Home",
+    page_icon="🏡",
 )
+st.markdown("<h1 style='text-align: center; font-size: 20px;'>Gurugram Flat and House Price Prediction with Feature Analysis and Recommendation System 🏡</h1>", unsafe_allow_html=True)
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("**Description**: Our project leverages advanced machine learning techniques to predict real estate prices for flats and houses in Gurugram, India. We have meticulously analyzed various features such as location, square footage, amenities, and historical pricing trends to build a robust prediction model.")
 
-st.sidebar.success("Select a demo above.")
+
+st.markdown("<h1 style='text-align: center; font-size: 20px;'>Impact</h1>", unsafe_allow_html=True)
+
+st.write("Our project empowers both buyers and sellers in Gurugram's real estate market by providing valuable insights into property pricing trends. The recommendation system enhances the property search experience, making it easier for buyers to find their ideal homes.")
+
+st.markdown("<h1 style='text-align: center; font-size: 20px;'>Future Enhancements:</h1>", unsafe_allow_html=True)
+
+st.write("In the future, we plan to incorporate more data sources, such as economic indicators and local development projects, to further improve the accuracy of our predictions. We also aim to expand our recommendation system's capabilities to include more sophisticated algorithms and personalization options.")
+
+st.markdown("<h1 style='text-align: center; font-size: 20px;'>Conclusion</h1>", unsafe_allow_html=True)
+
+st.write("The Gurugram Flat and House Price Prediction project not only facilitates more informed real estate decisions but also serves as a valuable tool for anyone interested in the dynamic property market of Gurugram.")
+
 
